44e92697aeacc4c175e4b63f01cbafb6fbf98c32
Michael Leblanc <mleblanc1@werfen.com>
2023-09-13 20:13:25 -0400

    figured out how to create route for static html files, created route for * and /notes

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -3,6 +3,7 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
+let submitButton = document.querySelector('.submit')
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
+//Express middlewear
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Test route for an example
 
-app.get('/', (req, res) => {
-    res.send('This is a test before I start the real work of creating routes!')
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    console.log('IT WORKED FOR NOTES!!!')
 });
 
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
 app.listen(PORT, () => {
-    console.log(`Server is listening at port ${PORT}`);
+    console.log(`Server is listening at http://localhost:${PORT}`);
 })
