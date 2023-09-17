35f7b7c29eb81ae970a7dd8c7fa5a7dc356c5ec7
Michael Leblanc <mleblanc1@werfen.com>
2023-09-17 18:54:27 -0400

    added route to make a post request and post a new comment

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 const notesData = require("./db/db");
+import {v4 as uuidv4} from 'uuid';
+let uuid = uuidv4();
 
 //Express middlewear
 
@@ -20,9 +22,48 @@ app.get('/notes', (req, res) => {
 app.get("/api/notes", (req, res) => res.status(200).json(notesData));
 
 app.post('/api/notes', (req, res) => {
+    //log that a post request was received
+    console.log(`${req.method} request received to add a note to the application`)
+    //Destructing the request 
     const { title, text } = req.body;
-  console.log(title, text);
-})
+    //Check to see that a title and text are available
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        }
+        //retrieve the existing notes saved in the JSON
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile('./db/reviews.json', JSON.stringify(parsedNotes),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated Notes!')
+        );
+            }
+        });
+        const response = {
+            status: 'success',
+            body: newReview,
+          };
+
+          console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
