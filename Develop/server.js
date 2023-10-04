b2264b04fa1c2a26fd28d4a1b1e1900ec2673dba
Michael Leblanc <mleblanc1@werfen.com>
2023-10-04 11:11:35 -0400

    fixed issue with PORT not wokring

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const { v4: uuidv4 } = require('uuid');
 
 //Express middlewear
